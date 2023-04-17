aad6dfed78a87e0a6f9e4888fb2c231d38161ae1
411017284 <s1101728@gm.pu.edu.tw>
2023-04-17 16:42:05 +0800

    第一次 Commit

--- app/src/main/java/tw/pu/edu/csim/ruby/introduce/MainActivity.kt
+++ app/src/main/java/tw/pu/edu/csim/ruby/introduce/MainActivity.kt
@@ -1,27 +1,47 @@
 package tw.pu.edu.csim.ruby.introduce
 
 import android.graphics.Color
+import android.graphics.Rect
 import android.graphics.Typeface
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.GestureDetector
 import android.view.GestureDetector.OnGestureListener
 import android.view.MotionEvent
 import android.view.View
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.bumptech.glide.Glide
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+public final class MyAppGlideModule : AppGlideModule()
 
 
-class MainActivity : AppCompatActivity(), OnGestureListener {
+
+class MainActivity : AppCompatActivity(), OnGestureListener, View.OnTouchListener {
 
     lateinit var txv: TextView
     lateinit var gDetector: GestureDetector
     var size: Float = 24f
     var count:Int = 0
+    lateinit var img1: ImageView
+    lateinit var img2: ImageView
+    lateinit var img3: ImageView
+    lateinit var mper: MediaPlayer
+    private lateinit var button: Button
+    private lateinit var imageView: ImageView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
         txv = findViewById(R.id.txv)
         txv.setTextColor(Color.parseColor("#FFC1E0"))
         txv.setBackgroundColor(Color.GRAY)
@@ -32,37 +52,124 @@ class MainActivity : AppCompatActivity(), OnGestureListener {
             )
         )
         txv.getBackground().setAlpha(150)  //0~255透明度值
-        gDetector = GestureDetector(this, this)
 
+        gDetector = GestureDetector(this, this)
+        img1 = findViewById(R.id.img1)
+        img2 = findViewById(R.id.img2)
+        img1.setOnTouchListener(this)
+        img2.setOnTouchListener(this)
+
+        img3 = findViewById(R.id.img3)
+        img3.visibility = View.GONE
+        Glide.with(this)
+            .load(R.drawable.me)
+            .into(img3)
+
+        mper = MediaPlayer()
+        mper = MediaPlayer.create(this, R.raw.lucky)
+        mper.start()
     }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        gDetector.onTouchEvent(event)
+        return true
+    }
+    override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+        if (v==img1){
+            txv.text = "這是我爸爸"
+        }
+        else{
+            txv.text = "這是我媽媽"
+        }
+        if (event?.action == MotionEvent.ACTION_MOVE){
+            v?.x = event.rawX- v!!.width/2
+            v?.y = event.rawY- v!!.height/2
+        }
+        var r1: Rect = Rect(img1.x.toInt(), img1.y.toInt(),
+            img1.x.toInt() + img1.width, img1.y.toInt() + img1.height)
+        var r2: Rect = Rect(img2.x.toInt(), img2.y.toInt(),
+            img2.x.toInt() + img2.width, img2.y.toInt() + img2.height)
+
+        if(r1.intersect(r2)) {
+            txv.text = "這是我!嘻嘻"
+            img3.visibility = View.VISIBLE
+        }
+        else{
+            img3.visibility = View.GONE
+        }
+
+        return true
+    }
+
 
     fun name(v: View) {
         val txv: TextView = findViewById(R.id.txv)
-        if (size > 20)
+        if (size > 5)
             size--
         findViewById<TextView>(R.id.txv).setTextSize(size)
         var myname: String
-        myname = "哈囉 大家好 我是陳柔涵"
+        myname = "哈囉 大家好 我是陳柔涵\n" +
+                "我的英文名字是Ruby\n" + "大家通常都叫我露比、嚕比或鹿比"
         txv.setText(myname)
     }
 
     fun years(v: View) {
         val txv: TextView = findViewById(R.id.txv)
-        if (size < 50)
+        if (size < 100)
             size++
         findViewById<TextView>(R.id.txv).setTextSize(size)
         var myname: String
-        myname = "我的生日是2003年6月1日!目前19歲"
+        myname = "2003年6月1日"
+        txv.setText(myname)
+    }
+
+    fun school(v: View) {
+        val txv: TextView = findViewById(R.id.txv)
+        if (size < 100)
+            size++
+        findViewById<TextView>(R.id.txv).setTextSize(size)
+        var myname: String
+        myname = "台南市和順國小\n" + "台南市私立港明國中\n" + "台南市私立港明高中"
         txv.setText(myname)
     }
 
     fun habit(v: View) {
         val txv: TextView = findViewById(R.id.txv)
-        if (size < 50)
-            size++
+        if (size > 5)
+            size--
         findViewById<TextView>(R.id.txv).setTextSize(size)
         var myname: String
         myname = "平常喜歡睡覺、聽音樂、唱歌和出去玩!"
+        txv.setText(myname)
+    }
+
+    fun food(v: View) {
+        val txv: TextView = findViewById(R.id.txv)
+        if (size > 5)
+            size--
+        findViewById<TextView>(R.id.txv).setTextSize(size)
+        var myname: String
+        myname = "我最喜歡的食物是冰淇淋!"
+        txv.setText(myname)
+    }
+
+    fun myself(v: View) {
+        val txv: TextView = findViewById(R.id.txv)
+        if (size > 5)
+            size--
+        findViewById<TextView>(R.id.txv).setTextSize(size)
+        var myname: String
+        myname = "外向、活潑、負責\n" + "我認為求學過程\n不僅可以學到課本的知識\n也可以在生活中尋找自己的方向"
+        txv.setText(myname)
+    }
+
+    fun talk(v: View) {
+        val txv: TextView = findViewById(R.id.txv)
+        if (size < 100)
+            size++
+        findViewById<TextView>(R.id.txv).setTextSize(size)
+        var myname: String
+        myname = "在努力和擺爛之間，選擇努力擺爛"
         txv.setText(myname)
     }
 
